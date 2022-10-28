e81b2d84943e2df7791f523eba6ad251f674dfd3
Florian Pasquet <florianpasquett@gmail.com>
2022-10-28 09:13:35 +0200

    card update

--- components/card.tsx
+++ components/card.tsx
@@ -9,15 +9,15 @@ interface CardInfo{
 }
 export default function Card(info: CardInfo) {
     return(
-        <div key={info.key} className="hover:scale-105 hover:shadow-lg transition-all duration-300 border rounded-lg min-h-[80vh]">
+        <div key={info.key} className="hover:scale-105 hover:shadow-lg transition-all duration-300 border rounded-lg min-h-[450px] max-h-[450px]">
             <Image src={(info.thumbnail)? info.thumbnail : ""} width="315" height="250" alt="test" className="w-full rounded-t-lg"/>
             <h3 className="text-center mt-2 font-semibold text-lg">{info.title}</h3>
 
-            <p className="px-3 text-center mt-3">
+            <p className="px-3 text-center mt-3 min-h-[80px]">
                 {info.description}
             </p>
 
-            <a href={info.link} className="p-3 bg-blue-400 rounded-lg border border-blue-500 text-white">Voir</a>
+            <a href={info.link} className="p-3 bg-blue-400 hover:bg-blue-500 transition-all duration-300 rounded-lg border border-blue-500 text-white text-center mx-auto block w-[80px]">Voir</a>
         </div>
     )
 }
