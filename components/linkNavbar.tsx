91009e0daf2c64498507b31dd910c5d1566fb1be
Florian Pasquet <florianpasquett@gmail.com>
2022-10-31 12:13:43 +0100

    update style when active

--- components/linkNavbar.tsx
+++ components/linkNavbar.tsx
@@ -8,7 +8,7 @@ interface LinkNavbarInfo {
 }
 export default function LinkNavbar(info: LinkNavbarInfo) {
     return (
-        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 rounded-2xl md:rounded-none hover:bg-blue-500 hover:text-white md:hover:scale-105 text-center py-1 transition-all duration-300 " + ((info.active)? "bg-blue-400 text-white translate-y-[-8px]" : "")}>
+        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 md:rounded-none hover:bg-blue-500 hover:text-white md:hover:scale-105 border-x text-center py-1 transition-all duration-300 bg-slate-100 " + ((info.active)? "translate-y-[-16px] border-t rounded-t-lg h-[111px]" : "")}>
             <li>
                 {info.name}
             </li>
