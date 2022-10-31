ca60995b574c454c73ed076e497a8aba0eb47afa
Florian Pasquet <florianpasquett@gmail.com>
2022-10-31 11:56:40 +0100

    style update when hover

--- components/linkNavbar.tsx
+++ components/linkNavbar.tsx
@@ -8,7 +8,7 @@ interface LinkNavbarInfo {
 }
 export default function LinkNavbar(info: LinkNavbarInfo) {
     return (
-        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 rounded-2xl md:rounded-none hover:bg-slate-200 md:hover:scale-105 hover:border-2 text-center py-1 transition-all duration-300 " + ((info.active)? "bg-blue-400 translate-y-[-8px]" : "")}>
+        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 rounded-2xl md:rounded-none hover:bg-blue-500 hover:text-white md:hover:scale-105 text-center py-1 transition-all duration-300 " + ((info.active)? "bg-blue-400 text-white translate-y-[-8px]" : "")}>
             <li>
                 {info.name}
             </li>
