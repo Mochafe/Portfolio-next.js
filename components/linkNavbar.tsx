72d342ec3b2edd66e886bfb2c018e6ded9a0bfca
Florian Pasquet <florianpasquett@gmail.com>
2022-10-31 13:16:36 +0100

    navbar translate update

--- components/linkNavbar.tsx
+++ components/linkNavbar.tsx
@@ -8,7 +8,7 @@ interface LinkNavbarInfo {
 }
 export default function LinkNavbar(info: LinkNavbarInfo) {
     return (
-        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 md:rounded-none hover:bg-blue-500 hover:text-white md:hover:scale-105 border-x text-center py-1 transition-all duration-300 bg-slate-100 " + ((info.active)? "translate-y-[-16px] border-t rounded-t-lg h-[80px]" : "")}>
+        <Link href={(info.href) ? info.href : "#"} onClick={(info.onClick)? info.onClick : undefined} className={"md:w-auto md:h-auto flex flex-col justify-center p-3 md:rounded-none hover:bg-blue-500 hover:text-white md:hover:scale-105 border-x text-center py-1 transition-all duration-300 bg-slate-100 " + ((info.active)? "md:translate-y-0 translate-y-[-16px] border-t rounded-t-lg md:rounded-none h-[80px] md:h-auto" : "")}>
             <li>
                 {info.name}
             </li>
