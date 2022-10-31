2eacebfdc3848d7a7714ac89a7b2cf2d5a98c21e
Florian Pasquet <florianpasquett@gmail.com>
2022-10-31 11:57:02 +0100

    active link update with link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,24 +2,51 @@ import Link from "next/link"
 import Image from "next/image"
 import LinkNavbar from "./linkNavbar"
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+interface Active{
+    home: boolean,
+    works: boolean
+}
 
 export default function Navbar() {
-    const [url, setUrl] = useState("");
+    const [active, setActive] = useState<Active>({
+        home: true,
+        works: false
+    })
+    let urlTest = useRouter().pathname;
 
-    function updateActive() {
-        setUrl(useRouter().pathname);
-        console.log(url);
-    }
+
+    useEffect(() => {
+        switch(urlTest) {
+            case "/":
+                setActive(
+                    {
+                        home: true,
+                        works: false
+                    }
+                );
+            break;
+            case "/works":
+                setActive(
+                    {
+                        home: false,
+                        works: true
+                    }
+                )
+        }
+    }, [useRouter().pathname])
+
+
+
     return (
         <nav className="bg-slate-100 border-t h-24 w-screen md:h-screen md:w-28 fixed bottom-0 flex md:flex-col border-r z-10">
-            <Link href="/" className="hidden md:block" onClick={updateActive}>
+            <Link href="/" className="hidden md:block">
                 <Image className="md:mx-auto mt-3" src="/logo_florian_pasquet.png" width="100" height="73" alt="logo" draggable="false" />
             </Link>
             <ul className="my-3 md:mt-[30vh] w-full flex md:flex-col gap-4 justify-center md:border-y text-xl">
-                <LinkNavbar href="/" name="Accueil" onClick={updateActive} />
-                <LinkNavbar href="/works" name="Projets" onClick={updateActive} />
+                <LinkNavbar href="/" name="Accueil" active={active.home}/>
+                <LinkNavbar href="/works" name="Projets" active={active.works}/>
             </ul>
         </nav>
     )
