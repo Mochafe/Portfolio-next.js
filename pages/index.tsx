77bbb2bc4beb4853ffb9455146af15a71c9830ad
Florian Pasquet <florianpasquett@gmail.com>
2022-11-07 15:48:45 +0100

    refacto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
 import type { NextPage } from 'next'
+import Typewriter from "typewriter-effect";
+import LocomotiveScroll from "locomotive-scroll"
+import { useState, useEffect } from 'react';
 
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+
+function  Home() {
+  /*
+  if(document === undefined) {
+    console.log("Undefineddd")
+  }
+  
+  const container = document.getElementById("data-scroll-container");
+
+  if(container !== null) {
+    const scroll = new LocomotiveScroll({
+      el: container,
+      smooth: true
+    });
+*/
+  
+  
+
   return (
-    <div>
+    <div id="data-scroll-container">
       <main>
-        <h1>Accueil</h1>
+        <section className='text-6xl ml-44 mt-36'>
+          <h2>Florian Pasquet</h2>
+          <p>Je suis </p><Typewriter options={{
+            strings: ["un développeur", "", ""],
+            autoStart: true,
+            loop: true
+          }} />
+        </section>
+
       </main>
     </div>
   )
 }
+
 
 export default Home
