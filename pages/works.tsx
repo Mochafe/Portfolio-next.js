cf0483f8d184de6f3e25fbd78cf80ae7c656518a
Florian Pasquet <florianpasquett@gmail.com>
2022-11-08 08:52:31 +0100

    setup placeholder card when loading

--- pages/works.tsx
+++ pages/works.tsx
@@ -25,38 +25,42 @@ export default function Works() {
             )
         }
     }
+
+    function CardPlaceholder(number: number) {
+        let placeholder = [];
+
+        for(let i = 0; i < number; i++) {
+            placeholder.push(<Card placeholder={true} />)
+        }
+
+        return placeholder;
+    }
     const [games, setGames] = useState<[]>([]);
     const [repos, setRepo] = useState<[]>([]);
     const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
-        let promises: Promise<Response | void>[] = [];
         setLoading(true);
-        promises[0] = fetch("/api/games").then((res) => {
+        fetch("/api/games").then((res) => {
             res.json().then((games) => {
                 setGames(games);
             });
-        promises[1] = fetch("/api/repos").then((res) => {
+        fetch("/api/repos").then((res) => {
             res.json().then(repo => {
                 setRepo(repo);
+            setLoading(false);
             })
         })
         });
-
-        Promise.all(promises).then(() => {
-            setLoading(false);
-
-        })
     }, [])
 
     return (
         <main>
             <h1 className="font-bold text-center text-2xl">Projets</h1>
             <div className="w-[90%] min-h-[33vh] mb-32 md:mb-0 mx-auto p-4 border rounded-lg grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-                <Card placeholder={true} />
                 { CardGames() }
                 { CardRepo() }
-                <span className="animate-spin">{ (isLoading)? "Loading" : "" }</span>
+                { (isLoading)? CardPlaceholder(10) : "" }
             </div>
 
         </main>
